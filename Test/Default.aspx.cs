b27863a5de5068660c094e0cb84672f331568367
martinez.ocr <oscar19@live.com.ar>
2020-01-20 20:06:35 -0300

    test de la generaciòn del pdf

---
+++ Test/Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using FacturaAfipPdf;
+
+namespace Test
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            FacturaAfipPdf.Print.BusinessInfo business = new Print.BusinessInfo();
+            business.Address = "Presidente Ibañez 129";
+            business.IVACondition = "IVA Responsable Inscripto";
+            business.GrossIncome = "207-123456-20";
+            business.CUIT = 30714460354;
+            business.InitialActivities = DateTime.Now.Date;
+            business.BusinessName = "adver infinity group";
+            business.PathImage = @"E:\Encabezado_FC.png";
+
+            FacturaAfipPdf.Print.ClientInfo client = new Print.ClientInfo()
+            {
+                Address = "Presidente Ibañez 129",
+                BusinessName = "asdadasda",
+                CUIT = 12345678910,
+                IVACondition = "Responsable inscripto",
+                SaleCondition = "Otra",
+                FirstName = "Oscar",
+                LastName = "Martinez",
+            };
+
+            HttpResponse response = HttpContext.Current.Response;
+            response.Clear();
+            response.ContentType = "application/pdf";
+            response.AppendHeader("Content-Disposition", "inline; filename=" + "Mi_pdf" + ".pdf"); //attachment
+            response.ContentType = "application/pdf";
+            response.BinaryWrite(FacturaAfipPdf.Print.ReceipToPdf(business,client));
+            response.End();
+        }
+    }
+}
